ffb05be8c4e0269187c12c5793e414d646ed4159
RobertoCapparelli <robertocapparelli2@gmail.com>
2024-10-23 09:18:04 +0200

    Fix error moveSquare

--- Assembly/SquareMove.asm
+++ Assembly/SquareMove.asm
@@ -9,16 +9,22 @@ start:
     STA $00 ; counter Lowbyte = 0
 
 loop:
+    LDA $00
+    CMP #$FF ; Speed
+    BNE increment_counter
+
     LDX $4000
     TXA
     AND #%00001111 ; enable only the input
-    BEQ next ; if there's no input go to next
+    BEQ change_color ; if there's no input go to next
 
-    LDA $00
-    CMP #$FF ; Speed
-    BNE increment_counter ; if counter < FF, increment it
+     ; if counter < FF, increment it
     
     JMP handle_direction ; if counter is FF, check for direction input
+change_color:
+    LDA #$01
+    STA $0200, Y
+    JMP next
 
 increment_counter:
     INC $00 ; Incrementa l'iteratore
@@ -44,49 +50,50 @@ handle_direction:
     JMP next
 
 up:
-    JSR clear_square
+    JSR clear_square_and_reset_counter
     TYA
     SEC ; Set carry = 1
     SBC #$10
     TAY
     JSR move_square
-    RTS
+    JMP next
 
 down:
-    JSR clear_square
+    JSR clear_square_and_reset_counter
     TYA
     CLC ; Clear carry
     ADC #$10
     TAY 
     JSR move_square
-    RTS
+    JMP next
 
 left:
-    JSR clear_square
+    JSR clear_square_and_reset_counter
     TYA
     SEC ; Set carry = 1
     SBC #$01
     TAY
     JSR move_square
-    RTS
+    JMP next
 
 right:
-    JSR clear_square
+    JSR clear_square_and_reset_counter
     TYA
     CLC ; Clear carry
     ADC #$01
     TAY
     JSR move_square
-    RTS
+    JMP next
 
 move_square:
-    LDA #$01
+    LDA #$03
     STA $0200, Y
     RTS
 
-clear_square:
+clear_square_and_reset_counter:
     LDA #$00
     STA $0200, Y
+    STA $00
     RTS
 
 next:
